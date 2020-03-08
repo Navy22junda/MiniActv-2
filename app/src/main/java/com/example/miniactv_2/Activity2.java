77abf911e8ae729bf405c613f594001fcf5d5b1e
Navy22junda <navy22junda@gmail.com>
2020-03-08 17:52:12 +0100

    Done intent that links first and second Activity

--- app/src/main/java/com/example/miniactv_2/Activity2.java
+++ app/src/main/java/com/example/miniactv_2/Activity2.java
@@ -1,6 +1,7 @@
 package com.example.miniactv_2;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,15 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        int repetitions = getIntent().getIntExtra("repetitions", -1);
+        String message = getIntent().getStringExtra("message");
+
+        TextView textView = (TextView)findViewById(R.id.textView2);
+        textView.setText(message);
+        for (int i = 1; i < repetitions; i++){
+            textView.append(message);
+        }
 
 
     }
--- app/src/main/java/com/example/miniactv_2/MainActivity.java
+++ app/src/main/java/com/example/miniactv_2/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.miniactv_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextBye;
+    private EditText editTextRepetitions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,17 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Els missatges transparents que es mostraran als textEdit
-        EditText editTextBye = (EditText)findViewById(R.id.editTextBye);
+        editTextBye = (EditText)findViewById(R.id.editTextBye);
         editTextBye.setHint("Introduce the bye message");
 
-        EditText editTextRepetitions = (EditText)findViewById(R.id.editTextRepetitions);
+        editTextRepetitions = (EditText)findViewById(R.id.editTextRepetitions);
         editTextRepetitions.setHint("Introduce a number");
 
-        //Guardo els valors que s'introdueixen
-        String message = editTextBye.getText().toString();
-        String repetitions = editTextRepetitions.getText().toString();
-        int rep = Integer.parseInt(repetitions);
+        //Lligo el buton amb la seguent activity
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //Guardo els valors que s'introdueixen
+                String message = editTextBye.getText().toString();
+                String repetitions = editTextRepetitions.getText().toString();
+                int rep = Integer.parseInt(repetitions);
+
+                Intent i = new Intent(MainActivity.this, Activity2.class);
+                i.putExtra("message", message);
+                i.putExtra("repetitions",rep);
+                startActivity(i);
+            }
+        });
 
     }
 }
